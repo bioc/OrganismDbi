1c79bdfe6f703b00541925e096f9fea99a8e189f
Marc Carlson <mcarlson@fhcrc.org>
2012-08-29 00:17:54 +0000

    bug fix to save the matrix to the file.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/OrganismDbi@68881 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/createOrganismPackage.R
+++ R/createOrganismPackage.R
@@ -85,7 +85,7 @@ makeOrganismPackage <- function(pkgname,
    ## other stuff) in /data as a serialized R file.
    ## There will already be a /data dir in the template
    ## So just save to it:
-   save(graphData, file=file.path(destDir,pkgname,"data","graphData.Rda"))
+   save(gd, file=file.path(destDir,pkgname,"data","graphData.Rda"))
 }
 
 
